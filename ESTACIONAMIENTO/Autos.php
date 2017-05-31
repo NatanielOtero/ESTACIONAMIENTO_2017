7817ca54fcd1e54c3c8c1f300f2ef2a7eb191054
NatanielOtero <natanielotero@gmail.com>
2017-05-30 22:22:58 -0300

    Frontend iniciado
    primera parte del frontend

--- ESTACIONAMIENTO/Autos.php
+++ ESTACIONAMIENTO/Autos.php
@@ -8,13 +8,15 @@ class Auto
   private $patente;
   private $marca;
   private $color;
-  
+  private $estadia;
 
-  function __construct($pat,$marc,$col)
+
+  function __construct($pat,$marc,$col,$est)
   {
     $this->patente = $pat;
     $this->marca = $marc;
     $this->color = $col;
+    $this->estadia = $est;
   }
 }
 
